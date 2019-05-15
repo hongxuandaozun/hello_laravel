2a49c1a6edf3cc60bccfc61cd14f0d8e405bdea5
hongxuan <18981914907@189.cn>
2019-05-15 17:18:38 +0800

    基础页面

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,13 +6,18 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    public function home(){
-    	return '主页';
+    public function home()
+    {
+        return view('static_pages/home');
     }
-    public function help(){
-        return 'help';
+
+    public function help()
+    {
+        return view('static_pages/help');
     }
-    public function about(){
-        return 'about';
+
+    public function about()
+    {
+        return view('static_pages/about');
     }
 }
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', 'about')
+@section('content')
+    <h1>abouts</h1>
+@stop
