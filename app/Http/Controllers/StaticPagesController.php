00de98c6869a77e5eda4fb3b5aa91fc3781e1c2c
hongxuan <18981914907@189.cn>
2019-05-15 17:01:55 +0800

    添加静态页面

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    public function home(){
+    	return '主页';
+    }
+    public function help(){
+        return 'help';
+    }
+    public function about(){
+        return 'about';
+    }
+}
